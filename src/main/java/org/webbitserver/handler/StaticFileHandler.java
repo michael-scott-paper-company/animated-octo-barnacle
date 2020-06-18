5057e1103641ab2bcc0c54443c5893faf081dd3e
Mario Campos <mario.andres.campos@gmail.com>
2020-06-18 15:39:29 -0500

    Ok, this one should cause ArrayIndexOutOfBounds

--- src/main/java/org/webbitserver/handler/StaticFileHandler.java
+++ src/main/java/org/webbitserver/handler/StaticFileHandler.java
@@ -106,7 +106,7 @@ public class StaticFileHandler extends AbstractResourceHandler {
                 byte[] array = md.digest(md5.getBytes("UTF-8"));
                 StringBuffer sb = new StringBuffer();
                 for (int i = 0; i < array.length; ++i) {
-                    sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+                    sb.append(Integer.toHexString((array[i*2] & 0xFF) | 0x100).substring(1, 3));
                 }
                 return sb.toString();
             } catch (Exception e) {
